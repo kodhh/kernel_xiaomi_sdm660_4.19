fc81b7d6a979353befd879b0eab5877c3517e579
kodhh <limw94@outlook.com>
2024-09-09 18:09:29 +0800

    Revert "mm: multi-gen LRU: call simple LMK if enabled"
    This reverts commit 3fc55fb061a54665ac68096c6e80b4a6d071fb1c.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -54,7 +54,6 @@
 #include <linux/shmem_fs.h>
 #include <linux/ctype.h>
 #include <linux/debugfs.h>
-#include <linux/simple_lmk.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -4032,9 +4031,6 @@ static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 	 * younger than min_ttl. However, another possibility is all memcgs are
 	 * either below min or empty.
 	 */
-#ifdef CONFIG_ANDROID_SIMPLE_LMK
-	simple_lmk_trigger();
-#else
 	if (!sc->order && mutex_trylock(&oom_lock)) {
 		struct oom_control oc = {
 			.gfp_mask = sc->gfp_mask,
@@ -4044,7 +4040,6 @@ static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 
 		mutex_unlock(&oom_lock);
 	}
-#endif
 }
 
 /*
